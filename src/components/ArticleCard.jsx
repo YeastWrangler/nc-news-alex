d1d4e2d42eba22c5c160968fb6745cf3fc3f9f03
YeastWrangler <102862775+YeastWrangler@users.noreply.github.com>
2022-08-23 15:02:47 +0100

    Added link for each individual article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import {patchArticleVote} from "../API"
 import {useState} from "react"
-import CommentCard from "./CommentCard"
+import {Link} from "react-router-dom"
 
 const ArticleCard = ({article}) => {
 
     const [voteCount, setVoteCount] = useState(article.votes);
     const [voteError, setVoteError] = useState("")
-
 
     const upVote = (event) => {
         setVoteCount((currCount) => currCount + 1)
@@ -26,7 +25,8 @@ const ArticleCard = ({article}) => {
     })
 }
 
-    return (<>
+    return (<> <Link to={`/articles/${article.article_id}`}>
+
         <div className="article-card">
             <h3>ID# {article.article_id} - {article.title}</h3>
             <p>Topic: {article.topic.charAt(0).toUpperCase() + article.topic.slice(1)}</p>
@@ -34,13 +34,14 @@ const ArticleCard = ({article}) => {
             <p className="article-body"> {article.body}</p>
             <p>Votes: {voteCount}  {voteError}  <br></br>Comments: {article.comment_count} </p>
         </div>
+            </Link>)
         <p> <button onClick={upVote} value={article.article_id} className="article-button"> Up Vote <span> 👍 </span> </button>
             <button onClick={downVote} value={article.article_id} className="article-button"> Down Vote <span> 👎 </span></button>
             <button className="article-button"> Add Comment <span> 📣 </span></button>
          
             </p>
-            <CommentCard article_id={article.article_id}/>
-   </>)
+           
+            </>)
 }
 
 export default ArticleCard
